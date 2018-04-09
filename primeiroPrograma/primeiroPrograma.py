8379d7b92f327b561b0895def41830a12719a0db
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-04-09 19:37:26 -0300

    Primeiro programa

---
+++ primeiroPrograma/primeiroPrograma.py
@@ -0,0 +1,9 @@
+###PRIMEIRO PROGRAMA EM PYTHON###
+a = int(input("Digite a sua idade"))
+
+if a < 20:
+	print ("Você é uma criança ou adolecente")
+else:
+	print ("Você já é um adulto")
+
+###PRIMEIRO PROGRAMA EM PYTHON###
