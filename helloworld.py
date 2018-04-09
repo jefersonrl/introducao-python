533b0ac78fbd1c084f951bba44bc06e8dd61a385
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-04-09 16:22:10 -0300

    Introdução ao Python - Operações Matemáticas

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,17 @@
 ### Comeco do codigo helloworld.py #############
 
 # Operação Matemática Básica
+# Adição
 print ("5 + 5 = ", 5 + 5)
+
+#Subtração
+print ("5 - 3 = ", 5 - 3)
+
+#Multiplicação
+print ("4 * 5 = ", 4 * 5);
+
+#Divisão
+print ("10 / 2 = ", 10 / 2);
 
 # Declaração de variáveis
 variavel = "Essa é a minha variável"
