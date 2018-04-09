9cbaada015616003244678e9564b9724375fa859
Jeferson Roberto de Lima <jefersonrl@icloud.com>
2018-04-09 16:15:40 -0300

    Introdução ao Python

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+### Comeco do codigo helloworld.py #############
+
+# Operação Matemática Básica
+print ("5 + 5 = ", 5 + 5)
+
+# Declaração de variáveis
+variavel = "Essa é a minha variável"
+print (variavel)
+
+# Concatenação de variáveis
+print ("Eu" +  " estou" + " testando")
+
+### Fim do Programa ############################
